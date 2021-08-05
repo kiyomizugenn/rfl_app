a4a78c9399eb7f2bdf5fb981c3922d659126dddf
Kiyomizu <kiyomizugenn@gamail.com>
2021-08-05 16:50:38 +0900

    Revert "Revert "create post function #3""
    This reverts commit accc9a2a4dc3a6ea9e92585ee653228dbc8853dc.

--- src/components/Feed.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { auth } from "../firebase/index";
-export const Feed = () => {
-  return (
-    <div>
-      <button onClick={() => auth.signOut()}>LOGOUT</button>
-    </div>
-  );
-};
