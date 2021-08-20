1555127bf3586d67d5bf82b5c576c34f4e091a8d
Kiyomizu <kiyomizugenn@gamail.com>
2021-08-21 02:01:47 +0900

    add post_form design #32

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./module.css/PostForm.module.css";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Avatar,
   Button,
   CssBaseline,
+  Card,
+  CardContent,
   TextField,
   Paper,
   Grid,
@@ -16,15 +19,28 @@ import {
   Box,
   MenuItem,
 } from "@material-ui/core";
-import AddPhotoIcon from "@material-ui/icons/Photo";
 import { useState, useEffect } from "react";
 import { auth, db, storage } from "../firebase";
 import firebase from "firebase";
 import { selectUser } from "../features/userSlice";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import TitleIcon from "@material-ui/icons/Title";
+import CategoryIcon from "@material-ui/icons/Category";
+import DetailsIcon from "@material-ui/icons/Details";
 
+const useStyles = makeStyles((theme) => ({
+  postFormBox: {
+    width: "95%",
+  },
+  postFormTextField: {
+    width: "100%",
+    marginBottom: "50px",
+    borderBottom: "2px solid #3333ff",
+  },
+}));
 const PostForm = () => {
+  const classes = useStyles();
   const user = useSelector(selectUser);
   const history = useHistory();
   const [title, setTitle] = useState("");
@@ -48,71 +64,87 @@ const PostForm = () => {
   };
 
   return (
-    <div style={{ marginTop: "80px" }}>
-      <form onSubmit={sendPost}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="title"
-          label="Title"
-          name="title"
-          autoComplete="current-title"
-          autoFocus
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="content"
-          label="Content"
-          type="content"
-          id="content"
-          autoComplete="current-content"
-          value={content}
-          onChange={(e) => {
-            setContent(e.target.value);
-          }}
-        />
-        <FormControl className={styles.postform_control}>
-          <InputLabel id="category-select-label">カテゴリー</InputLabel>
-          <Select
-            labelId="category-select-label"
-            id="category-select"
-            value={category}
-            onChange={(e) => {
-              setCategory(e.target.value);
-            }}
-          >
-            <MenuItem value="">None</MenuItem>
-            <MenuItem value="frontend">フロントエンドエンジニア</MenuItem>
-            <MenuItem value="backend">バックエンドエンジニア</MenuItem>
-            <MenuItem value="infra">インフラエンジニア</MenuItem>
-            <MenuItem value="designer">デザイナー</MenuItem>
-          </Select>
-        </FormControl>
-        <Button
-          variant="contained"
-          type="submit"
-          color="primary"
-          style={{ width: "100%", marginTop: "15px" }}
-        >
-          Post
-        </Button>
-        <div>
-          <button
-            onClick={async () => {
-              history.push("/");
-            }}
-          >
-            キャンセル
-          </button>
-        </div>
-      </form>
+    <div className={styles.post_form}>
+      <Card className={styles.post_form_card}>
+        <CardContent className={styles.post_form_container}>
+          <div className={styles.post_form_head}>
+            <h2>Post Form</h2>
+          </div>
+          <div className={classes.postFormBox}>
+            <form onSubmit={sendPost}>
+              <TextField
+                className={classes.postFormTextField}
+                required
+                id="title"
+                label="Title"
+                name="title"
+                type="text"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+              <br />
+
+              <FormControl className={styles.post_form_control}>
+                <InputLabel id="category-select-label" required>
+                  Category
+                </InputLabel>
+                <Select
+                  className={styles.post_form_select}
+                  labelId="category-select-label"
+                  id="category-select"
+                  value={category}
+                  onChange={(e) => {
+                    setCategory(e.target.value);
+                  }}
+                >
+                  <MenuItem value="">None</MenuItem>
+                  <MenuItem value="frontend">フロントエンドエンジニア</MenuItem>
+                  <MenuItem value="backend">バックエンドエンジニア</MenuItem>
+                  <MenuItem value="infra">インフラエンジニア</MenuItem>
+                  <MenuItem value="designer">デザイナー</MenuItem>
+                </Select>
+              </FormControl>
+              <br />
+
+              <TextField
+                className={classes.postFormTextField}
+                required
+                name="content"
+                label="Content"
+                type="text"
+                id="content"
+                multiline={true}
+                rows={4}
+                value={content}
+                onChange={(e) => {
+                  setContent(e.target.value);
+                }}
+              />
+
+              <Button
+                variant="contained"
+                type="submit"
+                color="primary"
+                style={{ width: "100%", marginTop: "15px" }}
+              >
+                Post
+              </Button>
+            </form>
+            <div>
+              <button
+                className={styles.post_form_btn}
+                onClick={async () => {
+                  history.push("/");
+                }}
+              >
+                キャンセル
+              </button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
