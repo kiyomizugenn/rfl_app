d025147214887b10721ce342b387fb06fba76852
Kiyomizu <kiyomizugenn@gamail.com>
2021-09-04 01:34:03 +0900

    delete followo btn Please enter the commit message for your changes. Lines starting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -7,6 +8,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import HeaderRight from "./HeaderRight";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,23 +17,40 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
     marginLeft: theme.spacing(7),
+    textDecoration: "none",
+    fontSize: "25px",
+    color: "#ffffff",
+    "&:hover": {
+      textDecoration: "none",
+      opacity: "0.5",
+    },
   },
   header: {
     width: "95%",
+  },
+  headerRight: {
+    margin: "0 0 0 auto",
   },
 }));
 
 export const Header = () => {
   const classes = useStyles();
+  const [update, setUpdate] = useState(false);
+
+  useEffect(() => {
+    setUpdate(update ? false : true);
+  }, []);
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar className={classes.header}>
-          <Typography variant="h5" className={classes.title}>
-            Reactive
-          </Typography>
           <div>
+            <Link to="/" className={classes.title}>
+              Reactive
+            </Link>
+          </div>
+          <div className={classes.headerRight}>
             <HeaderRight />
           </div>
         </Toolbar>
